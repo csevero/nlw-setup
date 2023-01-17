baea9502eb8d70a8b6a80883903ced61ee53f496
kissinger156 <severo.e.carlos@gmail.com>
2023-01-16 21:41:17 -0300

    adjust: changes on server to set cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,6 +5,8 @@ import { PrismaClient } from '@prisma/client';
 const app = Fastify();
 const prisma = new PrismaClient();
 
+app.register(cors)
+
 app.get('/', async () => {
   const habits = await prisma.habit.findMany();
 
