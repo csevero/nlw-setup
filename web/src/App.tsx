21d7f8af44bbda276605e118d61cd90bac35e692
kissinger156 <severo.e.carlos@gmail.com>
2023-01-16 21:40:25 -0300

    feature: setup the frontend project

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import { Habits } from './components/Habit';
+
+function App() {
+  return (
+    <div>
+      <Habits />
+      <Habits />
+      <Habits />
+      <Habits />
+    </div>
+  );
+}
+
+export default App;
---
+++ web/src/components/Habit.tsx
@@ -0,0 +1,3 @@
+export function Habits() {
+  return <h1 className="text-3xl font-bold underline bg-slate-600">Hello world!</h1>;
+}
