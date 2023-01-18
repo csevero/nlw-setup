56a916f270ab5051fc99901269f004b4266ee5a6
kissinger156 <severo.e.carlos@gmail.com>
2023-01-18 20:49:21 -0300

    feature: many changes on the front project
    Added new importants components, started the modal to add a new habit, added the popover on click on some allow habit card, added a progress bar component

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -26,7 +26,13 @@ export function SummaryTable() {
       </div>
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map(date => {
-          return <HabitDay key={date.toString()} />;
+          return (
+            <HabitDay
+              amount={5}
+              completed={0}
+              key={date.toString()}
+            />
+          );
         })}
 
         {amountOfDaysToFill > 0 &&
